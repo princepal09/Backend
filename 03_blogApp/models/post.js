d431b596811781cd4841a7f1575a0afb708a1f01
Prince Pal <princepal262501@gmail.com>
2026-01-13 20:57:29 +0530

    added all

--- 03_blogApp/models/post.js
+++ 03_blogApp/models/post.js
@@ -1,31 +1,28 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    title:{
-        type : String,
-        required : true,
-        maxLength : 50
+  title: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  body: {
+    type: String,
+    required: true,
+    maxLength: 500,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Like",
     },
-    body:{
-        type : String,
-        required : true,
-        maxLength : 500
-
+  ],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
     },
-    likes:[{
-        type: mongoose.Schema.Types.ObjectId,
-        ref :"Like"
-    }
+  ],
+});
 
-    ],
-    comments:[{
-        type: mongoose.Schema.Types.ObjectId,
-        ref :"Comment"
-    }
-
-    ]
-   
-})
-
-module.exports = mongoose.model("Post", postSchema)
+module.exports = mongoose.model("Post", postSchema);
